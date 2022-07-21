905e80cf52f0005f2ab6dd00136cfe61d51e4f80
Peter Whitby <peter.v.whitby@gmail.com>
2022-07-21 18:31:49 +0100

    starting equipment

--- entity.py
+++ entity.py
@@ -5,6 +5,7 @@ import math
 from typing import Optional, Tuple, Type, TypeVar, TYPE_CHECKING, Union
 from render_order import RenderOrder
 from components.level import Level
+from components.equipment import Equipment
 
 if TYPE_CHECKING:
     from components.ai import BaseAI
@@ -12,6 +13,7 @@ if TYPE_CHECKING:
     from components.consumable import Consumable
     from game_map import GameMap
     from components.inventory import Inventory
+    from components.equippable import Equippable
 
 T = TypeVar("T", bound="Entity")
 
@@ -92,6 +94,7 @@ class Actor(Entity):
         color: Tuple[int, int, int] = (255, 255, 255),
         name: str = "<Unnamed>",
         ai_cls: Type[BaseAI],
+        equipment: Equipment,
         fighter: Fighter,
         inventory: Inventory,
         level: Level,
@@ -107,7 +110,8 @@ class Actor(Entity):
         )
 
         self.ai: Optional[BaseAI] = ai_cls(self)
-
+        self.equipment: Equipment = equipment
+        self.equipment.parent = self
         self.fighter = fighter
         self.fighter.parent = self
         
@@ -132,7 +136,8 @@ class Item(Entity):
         char: str = "?",
         color: Tuple[int, int, int] = (255, 255, 255),
         name: str = "<Unnamed>",
-        consumable: Consumable,
+        consumable: Optional[Consumable] = None,
+        equippable: Optional[Equippable] = None,
     ):
         super().__init__(
             x=x,
@@ -145,4 +150,11 @@ class Item(Entity):
         )
 
         self.consumable = consumable
-        self.consumable.parent = self
+
+        if self.consumable:
+            self.consumable.parent = self
+
+        self.equippable = equippable
+
+        if self.equippable:
+            self.equippable.parent = self
