ebddcc8dc041a4497692dc0023ba61a90354b794
peter.whitby <peter.whitby@trackback.net>
2023-06-13 13:19:54 +0100

    pycham test

--- components/ai.py
+++ components/ai.py
@@ -3,11 +3,11 @@ from __future__ import annotations
 from typing import List, Optional, Tuple, TYPE_CHECKING
 import random
 import numpy as np  # type: ignore
-import tcod # type: ignore
+import tcod  # type: ignore
 from actions import Action, BumpAction, MeleeAction, MovementAction, WaitAction
+
 if TYPE_CHECKING:
     from entity import Actor
-
 
 
 class BaseAI(Action):
@@ -42,7 +42,7 @@ class BaseAI(Action):
 
         # Convert from List[List[int]] to List[Tuple[int, int]].
         return [(index[0], index[1]) for index in path]
-    
+
 
 class HostileEnemy(BaseAI):
     def __init__(self, entity: Actor):
@@ -57,16 +57,17 @@ class HostileEnemy(BaseAI):
 
         if self.engine.game_map.visible[self.entity.x, self.entity.y]:
             if distance <= 1:
-                return MeleeAction(self.entity, dx, dy).perform() # Will attack player when adjacent
+                return MeleeAction(self.entity, dx, dy).perform()  # Will attack player when adjacent
 
             self.path = self.get_path_to(target.x, target.y)
 
         if self.path:
             dest_x, dest_y = self.path.pop(0)
-            return MovementAction(self.entity, dest_x - self.entity.x, dest_y - self.entity.y,).perform()
+            return MovementAction(self.entity, dest_x - self.entity.x, dest_y - self.entity.y, ).perform()
 
         return WaitAction(self.entity).perform()
-    
+
+
 class ConfusedEnemy(BaseAI):
     """
     A confused enemy will stumble around aimlessly for a given number of turns, then revert back to its previous AI.
@@ -74,20 +75,21 @@ class ConfusedEnemy(BaseAI):
     """
 
     def __init__(
-        self, entity: Actor, 
-        previous_ai: Optional[BaseAI], 
-        turns_remaining: int):
+            self, entity: Actor,
+            previous_ai: Optional[BaseAI],
+            turns_remaining: int):
         super().__init__(entity)
 
         self.previous_ai = previous_ai
         self.turns_remaining = turns_remaining
 
-    def perform(self) -> None:  # Its possible the actor will just bump into the wall, wasting a turn. The actor will either try to move or attack in the chosen random direction.  
-        if self.turns_remaining <= 0: # Revert the AI back to the original state if the effect has run its course.
+    def perform(
+            self) -> None:  # Its possible the actor will just bump into the wall, wasting a turn. The actor will either try to move or attack in the chosen random direction.
+        if self.turns_remaining <= 0:  # Revert the AI back to the original state if the effect has run its course.
             self.engine.message_log.add_message(f"The {self.entity.name} is no longer confused.")
             self.entity.ai = self.previous_ai
         else:
-            direction_x, direction_y = random.choice( # Pick a random direction
+            direction_x, direction_y = random.choice(  # Pick a random direction
                 [
                     (-1, -1),  # Northwest
                     (0, -1),  # North
@@ -100,4 +102,4 @@ class ConfusedEnemy(BaseAI):
                 ]
             )
             self.turns_remaining -= 1
-            return BumpAction(self.entity, direction_x, direction_y,).perform()
+            return BumpAction(self.entity, direction_x, direction_y, ).perform()
